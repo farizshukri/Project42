431026b1c8bde294e8ff9ae2452ee603db668741
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2024-04-19 18:20:30 +0800

    initial commit

---
+++ PPTemp-configDB.js
@@ -0,0 +1,15 @@
+// backend/config/db.js
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/powerpoint-templates', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+module.exports = db;
